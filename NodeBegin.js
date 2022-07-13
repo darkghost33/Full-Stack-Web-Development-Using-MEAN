8b8c21b679cadf994625e57bb0b25ad9ceec5418
darkghost33 <deveshmukherjee76@gmail.com>
2022-07-13 09:42:22 +0530

    assign 4 working(Unaatempt not working)

--- NodeBegin.js
+++ NodeBegin.js
@@ -1,1 +1,66 @@
-console.log("Hello Welcome To NODE JS");
+// // // console.log("Hello Welcome To NODE JS");
+// // a = 10;
+// // b = 20;
+// // //console.log(a+b);
+// // console.log("Sum of " + a + " and " + b + " is= " + (a+b))
+
+// for(i=0; i<5; i++)
+// {
+//     console.log(i)
+// }
+
+// console.log(" ")
+
+// i=5
+// while(i!=0)
+// {
+//     console.log(i)
+//     i--
+// }
+
+// console.log(" ")
+
+// i=5
+// do
+// {
+//     console.log(i)
+//     i--
+// }while(i!=0)
+
+// console.log(" ")
+
+// if(i==0)
+//     console.log("IF Block")
+// else
+//     console.log("Else Block")    
+
+
+// arr = ["Pen","Pencil","Book","Scale"]
+// console.log(arr[2])
+// console.log("Aray elements are: -")
+// for(i=0; i<4; i++)
+// {
+//     console.log(arr[i])
+// }
+
+// student = {"Name":"ABC","Rollno":3001}
+// console.log(" ")
+// console.log(student)
+// console.log(" ")
+// console.log(student.Name)
+// console.log(student.Rollno)
+// console.log(" ")
+
+
+
+// studentobject = [
+//     {"Name":"ABC","Rollno":3001},
+//     {"Name":"LMN","Rollno":3002},
+//     {"Name":"XYZ","Rollno":3003}
+// ]
+// console.log(" ")
+// console.log(studentobject[0])
+// console.log(" ")
+// console.log(studentobject[1].Rollno)
+
+ex = require("http")
